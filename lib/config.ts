0b3487b1142235eefe29cba33a140183471799ed
Saurabh Prakash <saurabhp75@gmail.com>
2025-12-25 22:07:16 +0530

    Format file.

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,6 @@
-const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000").replace(/\/$/, "");
+const siteUrl = (
+	process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
+).replace(/\/$/, "");
 
 export const siteConfig = {
 	name: "code and context",
